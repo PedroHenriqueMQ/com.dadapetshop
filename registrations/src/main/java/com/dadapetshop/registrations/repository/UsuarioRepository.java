584eb3681d201c6cd65555647fcb487f00d0c2da
PedroHenriqueMQ <pedro_q777@live.com>
2024-11-18 12:22:16 -0300

    Adição de método findByEmail no UsuarioRepository

--- registrations/src/main/java/com/dadapetshop/registrations/repository/UsuarioRepository.java
+++ registrations/src/main/java/com/dadapetshop/registrations/repository/UsuarioRepository.java
@@ -1,10 +1,11 @@
 package com.dadapetshop.registrations.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dadapetshop.registrations.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-    
+    Optional<Usuario> findByEmail(String email);
 }
