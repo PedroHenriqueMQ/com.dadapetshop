f3b2277eb8cd80f7b465eb86b630c31f4404d613
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-11-18 15:59:21 -0300

    Implementação da classe UsuarioService

---
+++ registrations/src/main/java/com/dadapetshop/registrations/service/UsuarioService.java
@@ -0,0 +1,26 @@
+package com.dadapetshop.registrations.service;
+
+import org.springframework.stereotype.Service;
+
+import com.dadapetshop.registrations.dto.UsuarioDTO;
+import com.dadapetshop.registrations.mapper.UsuarioMapper;
+import com.dadapetshop.registrations.repository.UsuarioRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+    private UsuarioMapper usuarioMapper;
+
+    public void saveUsuario(UsuarioDTO usuarioDTO){
+        if (usuarioRepository.findByEmail(usuarioDTO.email()).isPresent()) 
+            throw new IllegalStateException ("Usuário com esse e-mail já existe.");
+
+        var usuario = usuarioMapper.toEntity(usuarioDTO);
+        usuarioRepository.save(usuario);
+    }
+
+
+}
