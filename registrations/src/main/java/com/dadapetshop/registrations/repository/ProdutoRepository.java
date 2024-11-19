441471735a4fdcdfbef7c44ba0edbd8b56a49d3f
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-11-18 23:07:13 -0300

    Implementação do mêtodo findByNome, na classe ProdutoRepository

--- registrations/src/main/java/com/dadapetshop/registrations/repository/ProdutoRepository.java
+++ registrations/src/main/java/com/dadapetshop/registrations/repository/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package com.dadapetshop.registrations.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dadapetshop.registrations.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-    
+    Optional<Produto> findByNome(String nome);
 }
