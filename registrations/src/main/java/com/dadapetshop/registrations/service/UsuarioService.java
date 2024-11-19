884257dfbc46f1475b7f5f1d01ac755f7fccee98
PedroHenriqueMQ <pedro_q777@live.com>
2024-11-18 21:34:26 -0300

    Implementando lógica de servição de adição de Pet

--- registrations/src/main/java/com/dadapetshop/registrations/service/UsuarioService.java
+++ registrations/src/main/java/com/dadapetshop/registrations/service/UsuarioService.java
@@ -14,11 +14,22 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
     private UsuarioMapper usuarioMapper;
 
-    public void saveUsuario(UsuarioDTO usuarioDTO){
-        if (usuarioRepository.findByEmail(usuarioDTO.email()).isPresent()) 
-            throw new IllegalStateException ("Usuário com esse e-mail já existe.");
+    public void saveUsuario(UsuarioDTO usuarioDTO) {
+        if(usuarioRepository.findByEmail(usuarioDTO.email()).isPresent())
+                throw new IllegalStateException("Usuário com esse e-mail já existe.");
 
         var usuario = usuarioMapper.toEntityBeforeSave(usuarioDTO);
         usuarioRepository.save(usuario);
+    }
+
+    public void addPet(UsuarioDTO usuarioDTO) {
+        var usuarioEncontrado = usuarioRepository.findByEmail(usuarioDTO.email())
+                .orElseThrow(() -> new IllegalStateException("Usuário não encontrado."));
+
+        var usuarioMapeado = usuarioMapper.toEntityAfterSave(usuarioDTO);
+        usuarioMapeado.getPets()
+                .forEach(novoPet -> usuarioEncontrado.getPets().add(novoPet));
+
+        usuarioRepository.save(usuarioEncontrado);
     }
 }
