8aca9b7bd539e87127fab51580e12c1571165af0
PedroHenriqueMQ <pedro_q777@live.com>
2024-11-20 13:25:59 -0300

    Atualizando produto no banco de dados

--- registrations/src/main/resources/db/migration/V1__create_produto_table.sql
+++ registrations/src/main/resources/db/migration/V1__create_produto_table.sql
@@ -4,5 +4,6 @@ CREATE TABLE produto (
     nome VARCHAR(255) NOT NULL,
     quantidade_estoque INT NOT NULL DEFAULT 0,
     quantidade_prateleira INT NOT NULL DEFAULT 0,
-    valor NUMERIC(10, 2) NOT NULL
+    valor NUMERIC(10, 2) NOT NULL,
+    categoria VARCHAR(20) NOT NULL
 );
