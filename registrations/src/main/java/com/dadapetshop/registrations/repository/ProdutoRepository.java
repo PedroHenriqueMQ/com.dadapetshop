27dbd10910244d55d3ceb7e6e91dd00169ad4380
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-11-20 16:25:35 -0300

    Atualização do método findByCategoria

--- registrations/src/main/java/com/dadapetshop/registrations/repository/ProdutoRepository.java
+++ registrations/src/main/java/com/dadapetshop/registrations/repository/ProdutoRepository.java
@@ -1,11 +1,10 @@
 package com.dadapetshop.registrations.repository;
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.dadapetshop.registrations.model.Produto;
+import com.dadapetshop.registrations.model.ProdutoCategoria;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     Optional<Produto> findByCodigo(String codigo);
+    Optional<Produto> findByCategoria(ProdutoCategoria categoria);
 }
