0dac80e65d12eb10a80bfffa407b9f3412733f55
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-11-20 10:05:34 -0300

    Implementação do método findByCpf

--- registrations/src/main/java/com/dadapetshop/registrations/repository/ProfissionalRepository.java
+++ registrations/src/main/java/com/dadapetshop/registrations/repository/ProfissionalRepository.java
@@ -1,9 +1,8 @@
 package com.dadapetshop.registrations.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.dadapetshop.registrations.model.Profissional;
+import java.util.Optional;
 
 public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
-    
+    Optional<Profissional>findByCpf(String cpf);
 }
