0ff163459f2c20fe865d5f6a67b881e8d8cfa742
PedroHenriqueMQ <pedro_q777@live.com>
2024-11-21 18:14:59 -0300

    Adicionando validations ao ProfissionalDTO

--- registrations/src/main/java/com/dadapetshop/registrations/dto/ProfissionalDTO.java
+++ registrations/src/main/java/com/dadapetshop/registrations/dto/ProfissionalDTO.java
@@ -1,7 +1,10 @@
 package com.dadapetshop.registrations.dto;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 public record ProfissionalDTO(
     String nome,
+    @CPF(message = "CPF fornecido não está no formato de CPF.")
     String cpf,
     String cargo
 ) {}
