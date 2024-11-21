477cb0b81b9e3d2b087b847392c9a1885367a134
PedroHenriqueMQ <pedro_q777@live.com>
2024-11-21 18:13:43 -0300

    Adicionando validations ao PetDTO

--- registrations/src/main/java/com/dadapetshop/registrations/dto/PetDTO.java
+++ registrations/src/main/java/com/dadapetshop/registrations/dto/PetDTO.java
@@ -4,11 +4,17 @@ import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+
 public record PetDTO(
     String nome,
     String raca,
+    @Min(message = "Idade não pode ser menor que 0.", value = 0)
     Integer idade,
+    @Min(message = "Peso não pode ser menor que 0.", value = 0)
     BigDecimal peso,
     @JsonProperty("email_tutor")
+    @Email(message = "Email fornecido não está no formato de email.")
     String emailTutor
 ) {}
