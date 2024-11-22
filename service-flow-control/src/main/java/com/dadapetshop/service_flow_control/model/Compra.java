180eeba8a07e448499aa736d1034de786295a11a
João <42324032+joao2212@users.noreply.github.com>
2024-11-22 08:52:56 -0300

    Implementação dos models

---
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/model/Compra.java
@@ -0,0 +1,29 @@
+package com.dadapetshop.service_flow_control.model;
+
+import com.dadapetshop.service_flow_control.dto.ProdutoDTO;
+import com.dadapetshop.service_flow_control.dto.ProfissionalDTO;
+import com.dadapetshop.service_flow_control.dto.UsuarioDTO;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Document(collection = "Compra")
+public class Compra {
+    @Id
+    private String id;
+    private String codigoFiscal;
+    @DBRef
+    private ProfissionalDTO atendente;
+    @DBRef
+    private UsuarioDTO usuarioComprou;
+    private List<ProdutoDTO> produtos = new ArrayList<>();
+    private BigDecimal valorTotal;
+}
---
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/model/Consulta.java
@@ -0,0 +1,33 @@
+package com.dadapetshop.service_flow_control.model;
+
+import com.dadapetshop.service_flow_control.dto.ProdutoDTO;
+import com.dadapetshop.service_flow_control.dto.ProfissionalDTO;
+import com.dadapetshop.service_flow_control.dto.UsuarioDTO;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Document(collection = "Consulta")
+public class Consulta {
+    @Id
+    private String id;
+    private String codigoFiscal;
+    @DBRef
+    private ProfissionalDTO veterinario;
+    @DBRef
+    private UsuarioDTO usuarioComprou;
+    private LocalDateTime horarioInicio;
+    private LocalDateTime horarioTermino;
+    private String status;
+    private List<ProdutoDTO> medicamentos = new ArrayList<>();
+    private BigDecimal valor;
+}
