8620b426233bcd83b564bf7c55833ca71c2e9dcd
João <42324032+joao2212@users.noreply.github.com>
2024-11-22 08:49:41 -0300

    Implementação da classe princial do projeto

---
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/ServiceFlowControlApplication.java
@@ -0,0 +1,13 @@
+package com.dadapetshop.service_flow_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFlowControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFlowControlApplication.class, args);
+	}
+
+}
