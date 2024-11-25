95343a723aee72461a2595cbcaf34b7918f08802
Gustavo Alves <gustavoalvesfla@hotmail.com>
2024-11-25 15:41:57 -0300

    Criação de Exception Customizada de Conversão

---
+++ registrations/src/main/java/com/dadapetshop/registrations/exception/ConversaoDeAtendimentoFalhaException.java
@@ -0,0 +1,7 @@
+package com.dadapetshop.registrations.exception;
+
+public class ConversaoDeAtendimentoFalhaException extends IllegalArgumentException {
+    public ConversaoDeAtendimentoFalhaException() {
+        super("Erro de conversão de Atendimento.");
+    }    
+}
