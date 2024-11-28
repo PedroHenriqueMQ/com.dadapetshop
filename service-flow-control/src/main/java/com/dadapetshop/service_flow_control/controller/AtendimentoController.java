d0cf63a5580ef9348fe21d04b529a2125bc5c30d
PedroHenriqueMQ <pedro_q777@live.com>
2024-11-28 16:41:22 -0300

    Atualização no Atendimento controller

--- service-flow-control/src/main/java/com/dadapetshop/service_flow_control/controller/AtendimentoController.java
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/controller/AtendimentoController.java
@@ -1,6 +1,8 @@
 package com.dadapetshop.service_flow_control.controller;
 
+import com.dadapetshop.service_flow_control.dto.ConsultaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +21,19 @@ public class AtendimentoController {
     private AtendimentoService atendimentoService;
 
     @PostMapping("/validate-service")
-    public ResponseEntity<String> validarProcedimento(@Valid @RequestBody Object atendimentoDTO) {
-        atendimentoService.validarAtendimento(atendimentoDTO);
-        return ResponseEntity.ok("Validação enviada com sucesso!");
+    public ResponseEntity<String> validarAtendimento(@Valid @RequestBody Object atendimentoDTO) {
+        Boolean aprovado = atendimentoService.validarAtendimento(atendimentoDTO);
+
+        if (aprovado == null)
+            return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("Timeout na resposta do serviço!");
+        else if (!aprovado)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Esse atendimento possui dados inválidos!");
+
+        if (atendimentoDTO instanceof ConsultaDTO)
+            atendimentoService.marcarConsulta(atendimentoDTO);
+        else
+            atendimentoService.marcarProcedimento(atendimentoDTO);
+
+        return ResponseEntity.ok("Atendimento marcado com sucesso!");
     }
 }
