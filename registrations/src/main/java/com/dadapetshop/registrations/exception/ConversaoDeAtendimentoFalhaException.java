9279560f30ae50d87c1b86aee380245a30f32553
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-11-27 21:16:59 -0300

    Removendo arquivos obsoletos

--- registrations/src/main/java/com/dadapetshop/registrations/exception/ConversaoDeAtendimentoFalhaException.java
+++
@@ -1,7 +0,0 @@
-package com.dadapetshop.registrations.exception;
-
-public class ConversaoDeAtendimentoFalhaException extends IllegalArgumentException {
-    public ConversaoDeAtendimentoFalhaException() {
-        super("Erro de conversão de Atendimento.");
-    }    
-}
