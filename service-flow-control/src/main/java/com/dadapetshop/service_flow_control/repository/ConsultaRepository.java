5c7d8d65e291ab61e4ebc27dd00a3badf87eaa8e
Gustavo Alves <gustavoalvesfla@hotmail.com>
2024-12-09 20:38:42 -0300

    Implementação de findByHorarioInicioBetween em consulta e procedimento repositories em service-flow-control

--- service-flow-control/src/main/java/com/dadapetshop/service_flow_control/repository/ConsultaRepository.java
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/repository/ConsultaRepository.java
@@ -3,9 +3,11 @@ package com.dadapetshop.service_flow_control.repository;
 import com.dadapetshop.service_flow_control.model.Consulta;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ConsultaRepository extends MongoRepository<Consulta, String> {
-    Optional<Consulta>findByCodigoFiscal(String codigoFiscal);
-    Optional<Consulta>findByValor(BigDecimal valor);
+    Optional<Consulta> findByCodigoFiscal(String codigoFiscal);
+    Optional<Consulta> findByValor(BigDecimal valor);
+    Optional<Consulta> findByHorarioInicioBetween(LocalDateTime horarioInicio, LocalDateTime horarioTermino);
 }
