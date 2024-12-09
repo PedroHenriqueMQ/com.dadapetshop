79066f8fa7faa4d8fa04b876f8bd74026c84641d
Gustavo Alves <gustavoalvesfla@hotmail.com>
2024-12-09 20:30:20 -0300

    Removendo constantes desnecessárias da aplicação service-flow-control

--- service-flow-control/src/main/java/com/dadapetshop/service_flow_control/constants/RabbitConstants.java
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/constants/RabbitConstants.java
@@ -4,6 +4,4 @@ public class RabbitConstants {
     public static final String EXCHANGE_NAME = "pet-exchange";
     public static final String PET_ATTENDANCE_QUEUE = "pet-attendance-queue";
     public static final String PRODUCT_PURCHASE_QUEUE = "product-purchase-queue";
-    public static final String PET_ATTENDANCE_ROUTING_KEY = "attendance.pet";
-    public static final String PRODUCT_PURCHASE_ROUTING_KEY = "attendance.product";
 }
