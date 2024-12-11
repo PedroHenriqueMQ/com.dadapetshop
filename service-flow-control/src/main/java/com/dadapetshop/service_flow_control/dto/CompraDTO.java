b6fd049d121802430f47afec01a9841599361ab6
João <42324032+joao2212@users.noreply.github.com>
2024-12-10 21:33:55 -0300

    Alteração do compraDTO para record em vez de class em service-flow-control

--- service-flow-control/src/main/java/com/dadapetshop/service_flow_control/dto/CompraDTO.java
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/dto/CompraDTO.java
@@ -5,24 +5,19 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.util.List;
 
-@Getter
-@Setter
-public class CompraDTO {
-    @JsonProperty("codigo_fiscal")
-    @NotBlank(message = "Campo código fiscal não pode estar vazio.")
-    private String codigoFiscal;
-    @NotBlank(message = "Campo cliente não pode estar vazio.")
-    private String cliente;
-    @NotEmpty(message = "Lista de produtos não pode estar vazia.")
-    private List<ProdutoDTO> produtos;
-    @JsonProperty("valor_total")
-    @Min(message = "Valor total não pode ser menor que 0.", value = 0)
-    @NotNull(message = "Campo valor total não pode ser nulo.")
-    private BigDecimal valorTotal;
-}
+public record CompraDTO (
+        @JsonProperty("codigo_fiscal")
+        @NotBlank(message = "Campo código fiscal não pode estar vazio.")
+        String codigoFiscal,
+        @NotBlank(message = "Campo cliente não pode estar vazio.")
+        String cliente,
+        @NotEmpty(message = "Lista de produtos não pode estar vazia.")
+        List<String> produtos,
+        @JsonProperty("valor_total")
+        @Min(message = "Valor total não pode ser menor que 0.", value = 0)
+        @NotNull(message = "Campo valor total não pode ser nulo.")
+        BigDecimal valorTotal
+) { }
