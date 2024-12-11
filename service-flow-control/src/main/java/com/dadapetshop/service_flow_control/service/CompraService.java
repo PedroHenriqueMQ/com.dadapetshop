f363c63f7ce34114d3e56f527d07f3476caaafc2
João <42324032+joao2212@users.noreply.github.com>
2024-12-10 21:36:09 -0300

    Implementação do compraService em service-flow-control

--- service-flow-control/src/main/java/com/dadapetshop/service_flow_control/service/CompraService.java
+++ service-flow-control/src/main/java/com/dadapetshop/service_flow_control/service/CompraService.java
@@ -3,18 +3,40 @@ package com.dadapetshop.service_flow_control.service;
 import com.dadapetshop.service_flow_control.constants.RabbitConstants;
 import com.dadapetshop.service_flow_control.dto.CompraDTO;
 import com.dadapetshop.service_flow_control.messenger.RabbbitMQProducer;
+import com.dadapetshop.service_flow_control.model.Compra;
+import com.dadapetshop.service_flow_control.repository.CompraRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CompraService extends RabbbitMQProducer {
+    private final CompraRepository compraRepository;
 
-    public CompraService(RabbitTemplate rabbitTemplate) {
+    @Autowired
+    public CompraService(RabbitTemplate rabbitTemplate, CompraRepository compraRepository) {
         super(rabbitTemplate);
+        this.compraRepository = compraRepository;
     }
 
-    public void validarCompra(CompraDTO compraDTO) {
-        sendMessage(RabbitConstants.PRODUCT_PURCHASE_QUEUE, compraDTO);
+    public boolean validarCompra(CompraDTO compraDTO) {
+        return sendMessageAndReceiveValidityConfirmation(RabbitConstants.PRODUCT_PURCHASE_QUEUE, compraDTO);
+    }
+
+    public boolean realizarCompra(CompraDTO compraDTO) {
+        if (compraRepository.findByCodigoFiscal(compraDTO.codigoFiscal()).isPresent())
+            return false;
+
+        var compra = Compra.builder()
+                .usuarioComprou(compraDTO.cliente())
+                .valorTotal(compraDTO.valorTotal())
+                .produtos(compraDTO.produtos())
+                .codigoFiscal(compraDTO.codigoFiscal())
+                .build();
+
+        compraRepository.save(compra);
+
+        return true;
     }
 }
