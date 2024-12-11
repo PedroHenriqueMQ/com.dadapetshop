bd3d6b53f775a7d98c89408c75b24984f1ef42a5
João <42324032+joao2212@users.noreply.github.com>
2024-12-10 21:31:31 -0300

    Adição de atributo na table produto no flyway migration em registrations

--- registrations/src/main/resources/db/migration/V1__create_produto_table.sql
+++ registrations/src/main/resources/db/migration/V1__create_produto_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE produto (
     id SERIAL PRIMARY KEY,
     codigo VARCHAR(50) UNIQUE NOT NULL,
+    caminho_imagem VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL,
     quantidade_estoque INT NOT NULL DEFAULT 0,
     quantidade_prateleira INT NOT NULL DEFAULT 0,
